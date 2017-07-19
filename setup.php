2dbdc2fee757d26cea89ceea391a8a1d92db8bf5
kim0thy <kimalvarez88@gmail.com>
2017-07-19 10:27:12 -0700

    hopefully this fixes my issue

--- setup.php
+++ setup.php
@@ -1,11 +1,9 @@
 <?php
-	$DB_DEST = "mysql:dbname=camagru;host=localhost";
-	$DB_DEBUG = "mysql:host=localhost";
-	$DB_USER = "root";
-	$DB_PASS = "jugzkwtn";
-	$DB_NAME = "camagru";
-	$DB_TABLE_USR = "user";
-	$DB_TABLE_IMG = "photo";
-	$DB_TABLE_COM = "comment";
-	$DB_PATH = "http://localhost:8080/";
+include('config.php');
+
+try {
+	$pdo = new PDO($DB_DEST, $DB_USER, $DB_PASS);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
 ?>
