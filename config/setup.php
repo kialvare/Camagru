5dbce812e183cf776fead88f1d2ad6d3cff81a07
kim0thy <kimalvarez88@gmail.com>
2017-07-24 10:29:09 -0700

    git rid

--- config/setup.php
+++ config/setup.php
@@ -3,10 +3,10 @@ include('config.php');
 
 try {
 	$pdo = new PDO($DB_DEST, $DB_USER, $DB_PASS);
-	$pdo->exec("CREATE DATABASE IF NOT EXISTS camagru; USE camagru");
+	$pdo->exec("CREATE DATABASE IF NOT EXISTS Camagru; USE Camagru");
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	#$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	#$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	
 	include('database.php');
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('config.php');
+include('config/setup.php');
 ?>
 <!DOCTYPE html>
 <html>
